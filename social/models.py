99954fc2d12c79581aa81f4661f7da127680cee9
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-06-09 09:08:45 +0300

    add profile page

--- social/models.py
+++ social/models.py
@@ -44,17 +44,23 @@ class Post(models.Model):
         return post
 
 class Profile(models.Model):
-    image = models.ImageField(upload_to='images')
-    bio = models.CharField(max_length=200)
-    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
-    
-    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.png', upload_to='profile_pics/')
+    bio = models.CharField(max_length=255)
+
     def __str__(self):
         return f'{self.user.username} Profile'
 
     class Meta:
-        db_table = 'Profile'
-    
+        db_table = 'profile'
+
+    @receiver(post_save, sender=User)
+    def update_create_profile(sender,instance,created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+
     def save_profile(self):
         self.save()
         
